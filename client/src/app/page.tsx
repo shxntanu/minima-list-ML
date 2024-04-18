ea02521daa59281a17a54789a5b7f8f4dd4676a5
Shantanu <shantanuwable2003@gmail.com>
2024-04-18 23:56:23 +0530

    update

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Card from "@/components/card/Card";
-import FlowerButton from "@/components/flower-btn/FlowerButton";
 import PromptBox from "@/components/prompt-box/prompt-box";
+import axios from "axios";
 
 export default function Home() {
     return (
@@ -10,47 +9,12 @@ export default function Home() {
             <div className="w-full h-full bg-white text-black">
                 <header className="flex items-center justify-between p-4">
                     <div className="flex items-center space-x-2">
-                        {/* <HomeIcon className="h-6 w-6" /> */}
                         <span className="text-lg font-bold">minima-List</span>
                     </div>
-                    <Button
-                        className="flex items-center space-x-2 text-white"
-                        variant="outline"
-                    >
-                        {/* <UserIcon className="h-6 w-6" /> */}
-                        <span>Sign In</span>
-                    </Button>
                 </header>
             </div>
 
             <main className="flex min-h-screen flex-col items-center p-24">
-                {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-                <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-                Get started by editing&nbsp;
-                <code className="font-mono font-bold">
-                src/app/page.tsx
-                </code>
-                </p>
-                <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-                <a
-                className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                By{" "}
-                <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className="dark:invert"
-                width={100}
-                height={24}
-                priority
-                />
-                </a>
-                </div>
-              </div> */}
-
                 <PromptBox />
                 <div className="grid grid-cols-5 space-x-2">
                     <button className="relative border hover:border-green-600 duration-500 group cursor-pointer text-sky-50  overflow-hidden h-14 w-56 rounded-md bg-green-800 p-2 flex justify-center items-center font-extrabold">
@@ -61,8 +25,7 @@ export default function Home() {
                         <div className="absolute z-10 w-16 h-16 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-green-500 delay-150 group-hover:delay-300"></div>
                         <p className="z-10">Happy</p>
                     </button>
-                    {/* <FlowerButton /> */}
-
+                    
                     <button className="relative border hover:border-gray-600 duration-500 group cursor-pointer text-sky-50  overflow-hidden h-14 w-56 rounded-md bg-gray-800 p-2 flex justify-center items-center font-extrabold">
                         <div className="absolute z-10 w-48 h-48 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-gray-900 delay-150 group-hover:delay-75"></div>
                         <div className="absolute z-10 w-40 h-40 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-gray-800 delay-150 group-hover:delay-100"></div>
