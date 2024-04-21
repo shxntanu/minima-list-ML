b801698186c7995213ad7bd6270ee169a69d7b2e
Shantanu <shantanuwable2003@gmail.com>
2024-04-21 22:00:22 +0530

    feat: add props

--- client/src/components/card/Card.tsx
+++ client/src/components/card/Card.tsx
@@ -1,33 +1,48 @@
 import styles from "./Card.module.css";
 import { Avatar } from "@nextui-org/react";
+import { Link } from "@nextui-org/react";
 
-function Card() {
+interface CardProps {
+    name: string;
+    artist: string;
+    album: string;
+    image: string;
+    externalURL: string;
+}
+
+const Card: React.FC<CardProps> = ({
+    name,
+    artist,
+    album,
+    image,
+    externalURL,
+}) => {
     return (
-        <div className={`${styles.card} flex flex-row justify-start items-center align-top mt-5`}>
-            <Avatar
-                isBordered
-                radius="sm"
-                src="https://i.pravatar.cc/150?u=a04258a2462d826712d"
-                className="ml-6 w-16 h-16 text-large"
-            />
-            <div className="ml-6">
-                <div className="flex flex-col justify-start items-start">
-                    <h1 className="text-xl font-bold">Closer</h1>
-                    <h2 className="text-lg font-medium">The Chainsmokers</h2>
-                </div>
-                <div className="flex flex-row justify-start items-center">
-                    <div className="flex flex-row justify-start items-center">
-                        <span className="text-gray-400">Album:</span>
-                        <span className="text-gray-600 ml-1">2016-07-29</span>
+        <Link href={externalURL}>
+            <div
+                className={`${styles.card} flex flex-row justify-start items-center align-top mt-5`}
+            >
+                <Avatar
+                    isBordered
+                    radius="sm"
+                    src={image}
+                    className="ml-6 w-16 h-16 text-large"
+                />
+                <div className="ml-6">
+                    <div className="flex flex-col justify-start items-start">
+                        <h1 className="text-xl font-bold">{name}</h1>
+                        <h2 className="text-lg font-medium">{artist}</h2>
                     </div>
-                    <div className="flex flex-row justify-start items-center ml-4">
-                        <span className="text-gray-400">Duration:</span>
-                        <span className="text-gray-600 ml-1">3 min 5 sec</span>
+                    <div className="flex flex-row justify-start items-center">
+                        <div className="flex flex-row justify-start items-center">
+                            <span className="text-gray-400">Album:</span>
+                            <span className="text-gray-600 ml-1">{album}</span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
-}
+};
 
 export default Card;
