7d790790a50877408201a49c8a757532eb4f0443
Shantanu <shantanuwable2003@gmail.com>
2024-04-21 22:00:13 +0530

    add backend data fetching

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,113 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import { useEffect, useState } from "react";
+import { Input } from "@/components/ui/input";
 import Card from "@/components/card/Card";
-import PromptBox from "@/components/prompt-box/prompt-box";
 import axios from "axios";
 
+interface Track {
+    name: string;
+    artist: string;
+    album: string;
+    image: string;
+    externalURL: string;
+}
+
 export default function Home() {
+    const [inputText, setInputText] = useState("");
+    const [year, setYear] = useState(new Date().getFullYear());
+    const [recommendations, setRecommendations] = useState<Track[]>([]);
+
+    //Request spotify Access token
+    useEffect(() => {
+        const requestToken = async () => {
+            try {
+                const response = await axios.post(
+                    "https://accounts.spotify.com/api/token",
+                    {
+                        grant_type: "client_credentials",
+                        client_id: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
+                        client_secret:
+                            process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET,
+                    },
+                    {
+                        headers: {
+                            "Content-Type": "application/x-www-form-urlencoded",
+                        },
+                    }
+                );
+
+                process.env.NEXT_PUBLIC_SPOTIFY_AUTHTOKEN =
+                    response.data.access_token;
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        requestToken();
+    }, []);
+
+    const handleSuggest = async () => {
+        setRecommendations([]);
+
+        const data = [
+            {
+                name: inputText,
+                year: year,
+            },
+        ];
+
+        try {
+            const response = await axios.post(
+                "http://localhost:8000/recommend",
+                data
+            );
+
+            console.log("RESPONSE: ", response);
+
+            const modelRecs: {
+                name: string;
+                year: number;
+                artists: string[];
+            }[] = JSON.parse(response.data);
+
+            console.log("MODEL RECOMMENDATIONS: ", modelRecs);
+
+            modelRecs.forEach(async (recommendation) => {
+                console.log("RECOMMENDATION: ", recommendation);
+                const queryText = `${
+                    recommendation.name
+                } artist:${recommendation.artists.join(" ")}`;
+
+                console.log("Query Text: ", queryText);
+                const response = await axios.get(
+                    `https://api.spotify.com/v1/search?q=${encodeURIComponent(
+                        queryText
+                    )}&type=track&limit=1`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${process.env.NEXT_PUBLIC_SPOTIFY_AUTHTOKEN}`,
+                        },
+                    }
+                );
+
+                const track = response.data.tracks.items[0];
+
+                const trackData = {
+                    name: track.name,
+                    artist: track.artists[0].name,
+                    album: track.album.name,
+                    image: track.album.images[0].url,
+                    externalURL: track.external_urls.spotify,
+                };
+
+                setRecommendations((prev) => [...prev, trackData]);
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <>
             <div className="w-full h-full bg-white text-black">
@@ -15,7 +119,45 @@ export default function Home() {
             </div>
 
             <main className="flex min-h-screen flex-col items-center p-24">
-                <PromptBox />
+                <div className="grid grid-cols-7 justify-center items-center w-full max-w-xl mx-auto space-x-3 px-5 pt-5">
+                    <div className="col-span-5 shadow-lg rounded-full">
+                        <Input
+                            className="  py-2 px-4 h-12"
+                            placeholder="Name the song that's stuck in your head..."
+                            value={inputText}
+                            type="text"
+                            onChange={(e) => setInputText(e.target.value)}
+                        />
+                    </div>
+                    <div className="col-span-2 shadow-lg rounded-full">
+                        <Input
+                            className="  py-2 px-4 h-12"
+                            placeholder="Year"
+                            type="number"
+                            value={year}
+                            defaultValue={year}
+                            onChange={(e) => setYear(parseInt(e.target.value))}
+                            onKeyPress={(e) => e.preventDefault()}
+                            onKeyDown={(e) =>
+                                e.key === "Backspace" && e.preventDefault()
+                            }
+                        />
+                    </div>
+                </div>
+                <div className="pt-3 pb-10">
+                    <button
+                        className="border text-gray-50  duration-300 relative group cursor-pointer   overflow-hidden h-12 w-48 rounded-lg bg-neutral-800 p-2  font-extrabold hover:bg-sky-700 col-span-2"
+                        onClick={handleSuggest}
+                    >
+                        <div className="absolute group-hover:-top-1 group-hover:-right-2 z-10 w-16 h-16 rounded-full group-hover:scale-150  duration-700 right-12 top-12 bg-yellow-500"></div>
+                        <div className="absolute group-hover:-top-1 group-hover:-right-2 z-10 w-12 h-12 rounded-full group-hover:scale-150  duration-700 right-20 -top-6 bg-orange-500"></div>
+                        <div className="absolute group-hover:-top-1 group-hover:-right-2 z-10 w-8 h-8   rounded-full group-hover:scale-150  duration-700 right-32 top-6 bg-pink-500"></div>
+                        <div className="absolute group-hover:-top-1 group-hover:-right-2 z-10 w-4 h-4   rounded-full group-hover:scale-150  duration-700 right-2 top-12 bg-red-600"></div>
+                        <p className="z-10 absolute bottom-2 left-2">
+                            Suggest?!
+                        </p>
+                    </button>
+                </div>
                 <div className="grid grid-cols-5 space-x-2">
                     <button className="relative border hover:border-green-600 duration-500 group cursor-pointer text-sky-50  overflow-hidden h-14 w-56 rounded-md bg-green-800 p-2 flex justify-center items-center font-extrabold">
                         <div className="absolute z-10 w-48 h-48 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-green-900 delay-150 group-hover:delay-75"></div>
@@ -25,7 +167,7 @@ export default function Home() {
                         <div className="absolute z-10 w-16 h-16 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-green-500 delay-150 group-hover:delay-300"></div>
                         <p className="z-10">Happy</p>
                     </button>
-                    
+
                     <button className="relative border hover:border-gray-600 duration-500 group cursor-pointer text-sky-50  overflow-hidden h-14 w-56 rounded-md bg-gray-800 p-2 flex justify-center items-center font-extrabold">
                         <div className="absolute z-10 w-48 h-48 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-gray-900 delay-150 group-hover:delay-75"></div>
                         <div className="absolute z-10 w-40 h-40 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-gray-800 delay-150 group-hover:delay-100"></div>
@@ -62,11 +204,17 @@ export default function Home() {
                         <p className="z-10">Focused</p>
                     </button>
                 </div>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                {recommendations.length > 0 &&
+                    recommendations.map((recommendation, index) => (
+                        <Card
+                            key={index}
+                            name={recommendation.name}
+                            artist={recommendation.artist}
+                            album={recommendation.album}
+                            image={recommendation.image}
+                            externalURL={recommendation.externalURL}
+                        />
+                    ))}
             </main>
         </>
     );
